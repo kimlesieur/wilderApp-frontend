2370e48b7fe04cecf78d6e89c39870e1707cdc5c
Kim Lesieur <lesieurkim@gmail.com>
2022-11-24 15:32:52 +0100

    feat: add error message, style: add autoAnimate library to the list

--- src/App.js
+++ src/App.js
@@ -3,18 +3,32 @@ import { getUsers } from './api';
 import './App.css';
 import Form from './components/Form';
 import Wilder from './components/Wilder';
+import { useAutoAnimate } from '@formkit/auto-animate/react';
 
 function App() {
+  const [parent, enableAnimations] = useAutoAnimate();
   const [users, setUsers] = useState([]);
+  const [newWilder, setNewWilder] = useState({});
+  const [error, setError] = useState(false)
 
   const fetchUsers = async () => {
-      const data = await getUsers();
-      setUsers(data);
+      await getUsers()
+      .then((data) => {
+        setUsers(data);
+      })
+      .catch((err) => {
+        setError(true);
+      });
   };
 
   useEffect(() => {
       fetchUsers();
-  }, []);
+  }, [newWilder]);
+
+  //function to update list when new wilder is created
+  const addWilderToState = (newWilder) => {
+    setNewWilder(newWilder);
+  };
 
 
   if(!users) {
@@ -29,9 +43,10 @@ function App() {
           </div>
         </header>
       <main className="container">
-        <Form />
+        <Form onWilderCreated={addWilderToState} />
         <h2>Wilders</h2>
-        <section className="card-row">
+        <section className="card-row" ref={parent}>
+          {error && <div>Erreur réseau</div>}
           {users && users.map((wilder) => {
             return (
               <Wilder name={wilder.name} skills={wilder.skills} key={wilder.id}/>
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,28 +2,51 @@ import {useState} from 'react';
 import { createUser } from '../api';
 
 
-const Form = () => {
+const Form = ({onWilderCreated}) => {
     const [wilderName, setWilderName] = useState("");
-    const handleInput = (e) => {
+    const [feedback, setFeedback] = useState(false);
+    const [error, setError] = useState(false);
+    const [processing, setProcessing] = useState(false);
+
+    const handleChange = (e) => {
         e.preventDefault();
+        setFeedback(false);
         setWilderName(e.target.value);
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(wilderName);
-        //post request here
-        const res = await createUser(wilderName);
-        console.log(res);
+        await createUser(wilderName)
+        .then((data) => {
+            setWilderName("");
+            setFeedback(true);
+            onWilderCreated(data);
+        })
+        .catch(err => {
+            setError(true);
+        })
+        
     };
 
     return (
         <div>
             <p>Créer un nouveau Wilder :</p>
             <form className="new-member-form" onSubmit={e => handleSubmit(e)}>
-                <input type="text" onChange={handleInput} value={wilderName} />
-                <button type="submit">Créer</button>
+                <label htmlFor="name" className=""/>
+                <span className=''>Nom : </span>
+                <input 
+                    type="text" 
+                    maxLength={30}
+                    id="name"
+                    onChange={handleChange} 
+                    value={wilderName}
+                    required
+                    disabled={processing}
+                />
+                <button type="submit">+</button>
             </form>
+            {feedback && <div>Wilder créé !</div>}
+            {error && <div>Erreur serveur</div>}
         </div>
     );
 };
